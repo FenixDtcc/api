dc135d9ed18cf05a6cf98c6a7b88499f57290711
IagoMartuci <martuciiago1992@gmail.com>
2023-05-27 01:08:33 -0300

    Atualização API - Mudança nos itens relacionados aos atendimentos.

--- QuantoDemoraApi/Models/Associado.cs
+++ QuantoDemoraApi/Models/Associado.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json.Serialization;
 
 namespace QuantoDemoraApi.Models
 {
